07ec76463925f5ab220e7271774ef25dfe6cdc3c
samiksha275 <agarwalsamiksha88@gmail.com>
2025-10-04 10:11:49 +0530

    TextInput added for name

--- hemocycle/App.js
+++ hemocycle/App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { TextInput, View, StyleSheet } from "react-native";
 
 export default function App() {
+  const [text, setText] = useState('')
   return (
     <View style={styles.container}>
-      <Text>Let's create a from</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder="Enter your name" value={text} onChangeText={setText}/>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex:1,
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
