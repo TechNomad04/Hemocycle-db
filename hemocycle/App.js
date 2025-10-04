a9056b0748fe4173b1d93a2b8a8e9c313dfe174f
samiksha275 <agarwalsamiksha88@gmail.com>
2025-10-04 13:31:04 +0530

    Upload image added

--- hemocycle/App.js
+++ hemocycle/App.js
@@ -1,11 +1,35 @@
 import { useState } from "react";
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Button, Image } from "react-native";
+import * as ImagePicker from 'expo-image-picker'
 
 export default function App() {
   const [text, setText] = useState('')
+  const [image, setImage] = useState(null)
+
+  const pickImage = async () => {
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if(!permissionResult.granted) {
+      alert('Permission not granted');
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    })
+
+    if(!result.canceled)
+      setImage(result.assets[0].uri)
+  }
+
   return (
     <View style={styles.container}>
       <TextInput placeholder="Enter your name" value={text} onChangeText={setText}/>
+      <Button title="Upload Picture" onPress={pickImage}/>
+      {image && <Image source={{uri: image}} style={styles.image}/>}
     </View>
   )
 }
@@ -15,5 +39,11 @@ const styles = StyleSheet.create({
     flex:1,
     alignItems: 'center',
     justifyContent: 'center'
-  }
+  },
+  image: {
+    width: 300,
+    height: 200,
+    borderRadius: 10,
+    marginTop: 20,
+  },
 })
