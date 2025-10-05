5f0fca0b171ede404784d45e73bcc53fdd832c96
samiksha275 <agarwalsamiksha88@gmail.com>
2025-10-05 23:39:09 +0530

    Database connected

--- hemocycle/backend/app.js
+++ hemocycle/backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const {connectdb} = require('./db')
 require('dotenv').config()
 
 app.use(express.json())
-
-app.listen(process.env.PORT, () => console.log("Server started"))
+connectdb().then(() => {
+    app.listen(process.env.PORT, () => console.log("Server and database connected"))
+}).catch(err => console.log(err))
+app.listen(process.env.PORT || 5000, () => console.log("Server started"))
