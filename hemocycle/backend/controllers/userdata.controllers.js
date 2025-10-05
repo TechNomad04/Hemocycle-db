d77e2c90b10ba35592f02ee2bad6df1e5d1aea0c
samiksha275 <agarwalsamiksha88@gmail.com>
2025-10-06 00:10:34 +0530

    adding data to db added

--- hemocycle/backend/controllers/userdata.controllers.js
+++ hemocycle/backend/controllers/userdata.controllers.js
@@ -1,9 +1,21 @@
+const User = require('../schema/userinfo.schema')
+
 const addData = async(req, res) => {
     try {
         const {name, category} = req.body
-        
+        if(!name || !category)
+            return res.status(400).json({status:false, message: "Missing details"})
+
+        const user = new User({name, category})
+        await user.save()
+
+        return res.status(200).json({status:true, details: {name, category}})
     } catch(err) {
         console.log(err)
         return res.status(500).json({status:false, message: "Internal server error"})
     }
+}
+
+module.exports = {
+    addData
 }
