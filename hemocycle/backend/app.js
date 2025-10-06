672e25654ec194bc40c3834f579e66b0384b023c
samiksha275 <agarwalsamiksha88@gmail.com>
2025-10-06 10:06:49 +0530

    Route added in app.js

--- hemocycle/backend/app.js
+++ hemocycle/backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const {connectdb} = require('./db')
+const userinfoRoutes = require('./routes/userinfo.routes')
 require('dotenv').config()
 
 app.use(express.json())
+
+app.use('/', userinfoRoutes)
 connectdb().then(() => {
     app.listen(process.env.PORT, () => console.log("Server and database connected"))
 }).catch(err => console.log(err))
