993ad9e00670c2afd59b3d94ea8efd665ed182bb
samiksha275 <agarwalsamiksha88@gmail.com>
2025-10-21 11:36:51 +0530

    Organised in the drive for the database

--- hemocycle/backend/controllers/userdata.controllers.js
+++ hemocycle/backend/controllers/userdata.controllers.js
@@ -38,8 +38,6 @@ const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_U
 const SCOPES = ['https://www.googleapis.com/auth/drive.file']
 const drive = google.drive({ version: 'v3', auth: oAuth2Client })
 
-const folderId = '1PuMvCtVIUdXtH4xALE4lJryn1R1vi000'
-
 if (fs.existsSync('tokens.json')) {
     const tokens = JSON.parse(fs.readFileSync('tokens.json'))
     oAuth2Client.setCredentials(tokens)
@@ -64,6 +62,32 @@ const uploadimage = async (req, res) => {
         const part = req.body.part
         const filePath = req.file.path
         const fileName = req.file.originalname
+        let folderId
+        const category = (await User.findById(id)).category
+        if(!category)
+            return res.status(404).json({status: false, message: "User doesn't exist"})
+        if (part == 'Conjunctiva') {
+            if(category == 'Non-Anemic')
+                folderId = '1ZbzorldZEoC16makUAZKDKqvZP_MY0IU'
+            else if (category == 'Mild')
+                folderId = '12jDT8YwhoK-q1ZcjpNcJpWaYjiYKcwOc'
+            else 
+                folderId = '1n7OKWiUKrTcwtR44JXuxqYetnLt0qJCe'
+        } else if (part == 'Fingernails') {
+            if(category == 'Non-Anemic')
+                folderId = '1mFrcD5vh6OC93u2DXVgUVKpQgp6gxT6G'
+            else if (category == 'Mild')
+                folderId = '1SOOfg0YAcC95JoWmg520YVw_g6zhVHDa'
+            else 
+                folderId = '1KW360HS86LLGkX7Su_07e3c3GMVjQna5'
+        } else {
+            if(category == 'Non-Anemic')
+                folderId = '1dxBgCNg705XZRWcnHjufDDCd7xtycmjf'
+            else if (category == 'Mild')
+                folderId = '1TmLsVTdWy4BJQpmWLRCSqoBMQSkR-ceW'
+            else 
+                folderId = '1ogM2qiBz3iBDzaPdt6Cu_mD2NK06z6ZT'
+        }
 
         const response = await drive.files.create({
             requestBody: {
